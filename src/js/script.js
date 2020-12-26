5a39687f714369918b70f07ca6fbb6a3e5d247ec
Timur <tim.sayfullaev@gmail.com>
2020-12-27 03:10:28 +0500

    bbccdd

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,45 @@
 $(document).ready(function () {
+    $(function () {
+        $("a[href^='#']").click(function () {
+            var _href = $(this).attr("href");
+            $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+            return false;
+        });
+    });
 
+    $('.brands__slider').slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        arrows: false,
+        cssEase: 'linear',
+        infinite: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        slidesToShow: 5,
+        speed: 8000,
+        swipe: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
