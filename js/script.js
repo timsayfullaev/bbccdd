f19db5b6a739828f3dfc043372ee7c4a7b9b0207
Timur <tim.sayfullaev@gmail.com>
2023-10-17 16:16:51 +0500

    counter

--- js/script.js
+++ js/script.js
@@ -1,25 +1,16 @@
-let calculate = document.getElementById("calculation");
-let count = document.getElementById("buttonCountNumber");
-calculation = document.getElementById("calculation").innerHTML;
-
-document.getElementById("buttonCountPlus").onclick = function () {
-    let countPlus = count.innerHTML;
-    if (+countPlus <= 3) {
-        count.innerHTML++;
-        let countPlus = count.innerHTML;
-        calculate.innerHTML = calculations(countPlus);
-    }
+function increaseCounter(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
 }
-
-document.getElementById("buttonCountMinus").onclick = function () {
-    let countMinus = count.innerHTML;
-    if (+countMinus >= 2) {
-        count.innerHTML--;
-        let countMinus = count.innerHTML;
-        calculate.innerHTML = calculations(countMinus);
+function decreaseCounter(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 0) {
+        value = isNaN(value) ? 0 : value;
+        value--;
+        input.value = value;
     }
-}
-
-calculations = (count) => {
-    return calculation + ` * ${count} = ` + (+count) * (+calculation);
 }
