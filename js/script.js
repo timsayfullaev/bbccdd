50f4d5e1d0bbdbe6972720840e7f55c25cce9e96
Timur <tim.sayfullaev@gmail.com>
2023-10-22 11:59:00 +0500

    replace font & remove catalog nav & add jquery and superfishmenu

--- js/script.js
+++ js/script.js
@@ -40,3 +40,10 @@ function decreaseCounter(a, b) {
     }
 }
 // COUNTER END
+
+jQuery(document).ready(function(){
+    jQuery('ul.sf-menu').superfish({
+        animation: {height:'show'},	// slide-down effect without fade-in
+        delay:		 1200			// 1.2 second delay on mouseout
+    });
+});
