8efe21161e232e8372bbe4858b27b3766cf3853a
Timur <tim.sayfullaev@gmail.com>
2023-10-23 15:10:03 +0500

    header

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-// CURRENCY BEGIN
 function currencyDropdown() {
-    const currency = document.querySelector( '.currency');
+    
+    const currency = document.querySelector('.currency');
     const currencyButton = document.querySelector('.currency-button');
     const currencyContent = document.querySelector('.currency-content');
-    
+
     currencyButton.classList.toggle('currency-button_active');
     currencyContent.classList.toggle('currency-content_active');
 
     document.addEventListener('click', (e) => {
+        
         const withinBoundaries = e.composedPath().includes(currency);
 
         if (!withinBoundaries) {
@@ -16,34 +17,48 @@ function currencyDropdown() {
         }
     });
 };
-// CURRENCY END
 
-// CATALOG BEGIN
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".content");
 
-// CATALOG END
+for (let i = 0; i < tabs.length; i++) {
 
-// COUNTER BEGIN
+    tabs[i].addEventListener("mouseover", ( event ) => {
+ 
+		let tabsChildren = event.target.parentElement.children;
+
+        for (let t = 0; t < tabsChildren.length; t++) {
+			tabsChildren[t].classList.remove("tab--active");
+		}
+
+        tabs[i].classList.add("tab--active");
+
+        let tabContentChildren = event.target.parentElement.nextElementSibling.children;
+
+        for (let c = 0; c < tabContentChildren.length; c++) {
+			tabContentChildren[c].classList.remove("content--active");
+		}
+
+        contents[i].classList.add("content--active");
+	});
+}
+
 function increaseCounter(a, b) {
     let input = b.previousElementSibling;
     let value = parseInt(input.value, 10);
+   
     value = isNaN(value) ? 0 : value;
     value++;
     input.value = value;
 }
+
 function decreaseCounter(a, b) {
     let input = b.nextElementSibling;
     let value = parseInt(input.value, 10);
+   
     if (value > 0) {
         value = isNaN(value) ? 0 : value;
         value--;
         input.value = value;
     }
 }
-// COUNTER END
-
-jQuery(document).ready(function(){
-    jQuery('ul.sf-menu').superfish({
-        animation: {height:'show'},	// slide-down effect without fade-in
-        delay:		 1200			// 1.2 second delay on mouseout
-    });
-});
